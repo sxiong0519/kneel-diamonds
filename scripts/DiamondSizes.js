302be33d00dea44f22e3eb823cb8be98f55d457f
sxiong0519 <sxiong0519@gmail.com>
2021-06-09 11:08:46 -0500

    setstyles and setsize

--- scripts/DiamondSizes.js
+++ scripts/DiamondSizes.js
@@ -1,11 +1,13 @@
-import { getSizes } from "./database.js"
+import { getSizes, setSize } from "./database.js"
 
 const sizes = getSizes()
 
 document.addEventListener(
     "change",
     (event) => {
-        
+        if (event.target.name === "size") {
+            setSize(parseInt(event.target.value))
+        }
     }
 )
 
--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -1,11 +1,13 @@
-import { getStyles } from "./database.js"
+import { getStyles, setStyle } from "./database.js"
 
 const styles = getStyles()
 
 document.addEventListener(
     "change",
     (event) => {
-        
+        if (event.target.name === "style") {
+            setStyle(parseInt(event.target.value))
+        }
     }
 )
 
